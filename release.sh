13e2811f50c6157c91f6b44a3ff344260edf4858
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-06-06 16:46:47 +0300

    .

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 # RUSTSEC-2024-0436 paste: rust_icu
 cargo audit --ignore RUSTSEC-2024-0436 && \
-  cargo build --release && \
+  cargo clippy && cargo build --release && \
   cargo release patch --no-publish --execute
